dc071948edf0203ad79ead7b31077456ed2b0bb1
Phenomenal4567 <86519932+Phenomenal4567@users.noreply.github.com>
2022-10-23 12:33:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+window.addEventListener("load", calculateTime)
+
+function calculateTime() {
+  let date = new Date(); 
+ let dayNumber = date.getDay();
+ let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let month = date.getMonth();
+  let dates = date.getDate();
+  let year = date.getFullYear();
+  let ampm = hour >= 12 ? "PM" : "AM";
+  let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  hour = hour % 12;
+  hour = hour ? hour : "12";
+  hour = hour < 10 ? "0" + hour : hour;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document.getElementById("month").innerHTML = months[month];
+  document.getElementById("day").innerHTML = dayNames[dayNumber];
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("minutes").innerHTML = minutes;
+ document.getElementById("dates").innerHTML = dates;
+  document.getElementById("year").innerHTML = year;
+  document.getElementById("ampm").innerHTML = ampm;
+  
+  setTimeout(calculateTime, 200);
+  
+}
